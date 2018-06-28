3d5f701337af2f4bd8fc9ab69612f7ad88d2a792
Hugo Escandon <hescandon6@gmail.com>
2018-06-28 14:09:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(katzDeli) {
   }
     var line =[]
     for ( var i = 0;i < katzDeli.length; i++){
-      line.push(` `+[i+1]+`. `  + katzDeli[i])
+      line.push(" "+[i+1]+". "  + katzDeli[i])
     }
 
 return("The line is currently:" + line)
